3a38b90e25237374a6594e28870d111236b8df54
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-24 02:02:27 +0530

    create a menu page and add css to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Menu from "./components/Menu.jsx"
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      
+    <>
+    <div className='App'>
+      <h1>Quiz App</h1>
+      <Menu />
     </div>
+    </>
   )
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Menu.css'
+const Menu = () => {
+  return (
+    <div className='menu-container'>
+     <input placeholder="Ex. Adolf Hitler" ></input>
+     <button>Start Quiz</button>
+    </div>
+  )
+}
+
+export default Menu
